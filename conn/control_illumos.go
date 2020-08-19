e06f6c5a7d2f6c90ba89b000883de9660a42449e
Nahum Shalman <nahamu@gmail.com>
2020-08-19 12:57:57 +0000

    XXX on illumos it seems that SO_REUSEADDR is NOT the same as SO_REUSEPORT

--- conn/control_illumos.go
+++ conn/control_illumos.go
@@ -1,4 +1,4 @@
-// +build !windows,!illumos
+// +build illumos
 
 package conn
 
@@ -18,12 +18,13 @@ func netControl(network, address string, conn syscall.RawConn) (err error) {
 	if err2 != nil {
 		return err2
 	}
+	// XXX on illumos it seems that SO_REUSEADDR is NOT the same as SO_REUSEPORT
 	// SO_REUSEADDR on Linux/Windows is equivalent ot SO_REUSEPORT on
 	// BSD/macOS, so we set it too on non-Linux systems.
 	// For a detailed explanation of the difference between these socket
 	// options on Linux and BSD see https://stackoverflow.com/a/14388707.
 	err2 = conn.Control(func(fd uintptr) {
-		err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+		err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
 	})
 	if err != nil {
 		return err
